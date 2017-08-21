d9b4fb25a7442968f1023c82897a8a4962f4229e
95A31 <x95a31xreg@gmail.com>
2017-08-21 08:21:38 +0200

    [Choosers] Add integer values chooser

---
+++ include/choosers/IntValuesChooser.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <variables/IntVariables.h>
+
+struct IntValuesChooser
+{
+    enum Type
+    {
+    };
+
+    int type;
+    IntVariables* variables;
+
+    void initialzie(int type, IntVariables* variables);
+
+    bool getFirstValue(int variable, int* firstValue);
+    bool getNextValue(int variable, int currentValue, int* nextValue);
+};
