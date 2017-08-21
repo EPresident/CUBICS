65ea0e6e5e69cd1cb4197dd0ffee8dfeef4acc9b
95A31 <x95a31xreg@gmail.com>
2017-08-21 06:57:25 +0200

    [Searchers] Add integer backtrack searcher

---
+++ include/searchers/IntBacktrackSearcher.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#include <variables/IntVariables.h>
+#include <constraints/IntConstraints.h>
+#include <choosers/IntVariablesChooser.h>
+#include <choosers/IntValuesChooser.h>
+#include <propagators/IntConstraintsPropagator.h>
+#include <searchers/IntBacktrackStack.h>
+
+struct IntBacktrackSearcher
+{
+    enum States
+    {
+        VariableNotChosen,
+        VariableChosen,
+        ValueChosen,
+        SuccessfulPropagation,
+        ValueChecked
+    };
+
+    int backtrackingState;
+    int backtrackingLevel;
+
+    int chosenVariable;
+    int chosenValue;
+    Vector<int> chosenVariables;
+    Vector<int> chosenValues;
+
+    IntVariables* variables;
+    IntConstraints* constraints;
+
+    IntBacktrackStack stack;
+
+    IntVariablesChooser variablesChooser;
+    IntValuesChooser valuesChooser;
+
+    IntConstraintsPropagator propagator;
+
+    void initialize(IntVariables* variables, IntConstraints* constraints);
+    void deinitialize();
+
+    bool getNextSolution();
+};
+
---
+++ src/searchers/IntBacktrackSearcher.cu
@@ -0,0 +1,139 @@
+#include <searchers/IntBacktrackSearcher.h>
+#include <utils/Utils.h>
+
+void IntBacktrackSearcher::initialize(IntVariables* variables, IntConstraints* constraints)
+{
+    this->variables = variables;
+    this->constraints = constraints;
+
+    chosenVariables.initialize(variables->count);
+    chosenValues.initialize(variables->count);
+
+    stack.initialize(&variables->domains.representations);
+
+    variablesChooser.initialzie(IntVariablesChooser::InOrder, variables, &chosenVariables);
+    valuesChooser.initialzie(IntValuesChooser::InOrder, variables);
+
+    propagator.initialize(variables, constraints);
+
+    backtrackingLevel = 0;
+    backtrackingState = VariableNotChosen;
+}
+
+void IntBacktrackSearcher::deinitialize()
+{
+    chosenVariables.deinitialize();
+
+    chosenValues.deinitialize();
+
+    stack.deinitialize();
+
+    propagator.deinitialize();
+}
+
+bool IntBacktrackSearcher::getNextSolution()
+{
+    bool solutionFound = false;
+
+    while (backtrackingLevel >= 0 and (not solutionFound))
+    {
+        switch (backtrackingState)
+        {
+            case VariableNotChosen:
+            {
+                stack.saveState(backtrackingLevel);
+
+                if (variablesChooser.getVariable(backtrackingLevel, &chosenVariable))
+                {
+                    chosenVariables.push_back(chosenVariable);
+                    backtrackingState = VariableChosen;
+                }
+                else
+                {
+                    LogUtils::error(__PRETTY_FUNCTION__, "Failed to set variable");
+                }
+            }
+                break;
+
+            case VariableChosen:
+            {
+                if (not variables->domains.isSingleton(chosenVariables.back()))
+                {
+
+                    if (valuesChooser.getFirstValue(chosenVariables.back(), &chosenValue))
+                    {
+                        chosenValues.push_back(chosenValue);
+                        variables->domains.fixValue(chosenVariables.back(), chosenValues.back());
+                        backtrackingState = ValueChosen;
+                    }
+                    else
+                    {
+                        LogUtils::error(__PRETTY_FUNCTION__, "Failed to set first value");
+                    }
+                }
+                else
+                {
+                    chosenValues.push_back(variables->domains.getMin(chosenVariables.back()));
+                    backtrackingState = SuccessfulPropagation;
+                }
+            }
+                break;
+            case ValueChosen:
+            {
+                bool noEmptyDomains = propagator.propagateConstraints();
+
+                if (noEmptyDomains)
+                {
+                    backtrackingState = SuccessfulPropagation;
+                }
+                else
+                {
+                    backtrackingState = ValueChecked;
+                }
+            }
+                break;
+
+            case SuccessfulPropagation:
+            {
+                if (backtrackingLevel < variables->count - 1)
+                {
+                    backtrackingLevel += 1;
+                    backtrackingState = VariableNotChosen;
+                }
+                else
+                {
+                    backtrackingState = ValueChecked;
+
+                    if (propagator.verifyConstraints())
+                    {
+                        solutionFound = true;
+                    }
+                }
+            }
+                break;
+
+            case ValueChecked:
+            {
+                stack.restoreState(backtrackingLevel);
+                if (valuesChooser.getNextValue(chosenVariables.back(), chosenValues.back(), &chosenValue))
+                {
+                    chosenValues.back() = chosenValue;
+                    variables->domains.fixValue(chosenVariables.back(), chosenValues.back());
+                    backtrackingState = ValueChosen;
+                }
+                else
+                {
+                    stack.clearState(backtrackingLevel);
+
+                    backtrackingLevel -= 1;
+                    chosenVariables.pop_back();
+                    chosenValues.pop_back();
+
+                }
+            }
+                break;
+        }
+    }
+
+    return solutionFound;
+}
