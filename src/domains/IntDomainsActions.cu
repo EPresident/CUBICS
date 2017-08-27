9d579e7ca8f5dcecea4a5bd593f4762a640cc95b
95A31 <x95a31xreg@gmail.com>
2017-08-27 16:34:29 +0200

    [Actions] Add integer domains actions

---
+++ src/domains/IntDomainsActions.cu
@@ -0,0 +1,59 @@
+#include <algorithm>
+
+#include <domains/IntDomainsActions.h>
+#include <utils/Utils.h>
+
+void IntDomainsActions::initialize(int count)
+{
+    elementsToRemove.initialize(count);
+
+    lowerbounds.initialize(count);
+    upperbounds.initialize(count);
+}
+
+void IntDomainsActions::deinitialize()
+{
+    for (int i = 0; i < elementsToRemove.size; i += 1)
+    {
+        elementsToRemove[i].deinitialize();
+    }
+    elementsToRemove.deinitialize();
+
+    lowerbounds.deinitialize();
+    upperbounds.deinitialize();
+}
+
+void IntDomainsActions::push()
+{
+    elementsToRemove.resize_by_one();
+    elementsToRemove.back().initialize();
+
+    lowerbounds.push_back(INT_MIN);
+    upperbounds.push_back(INT_MAX);
+}
+
+void IntDomainsActions::clear(int index)
+{
+    elementsToRemove[index].clear();
+
+    lowerbounds[index] = INT_MIN;
+    upperbounds[index] = INT_MAX;
+}
+
+void IntDomainsActions::removeElement(int index, int val)
+{
+    if (lowerbounds[index] <= val and val <= upperbounds[index])
+    {
+        elementsToRemove[index].push_back(val);
+    }
+}
+
+void IntDomainsActions::removeAnyGreaterThan(int index, int val)
+{
+    upperbounds[index] = std::min(val, upperbounds[index]);
+}
+
+void IntDomainsActions::removeAnyLesserThan(int index, int val)
+{
+    lowerbounds[index] = std::max(val, lowerbounds[index]);
+}
