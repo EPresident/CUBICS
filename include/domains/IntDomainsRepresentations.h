988a6c9fb0ce0d67c4daaaf9fc055bfc09032788
95A31 <x95a31xreg@gmail.com>
2017-09-24 22:52:02 +0200

    [Representations] Add integer domains representations

---
+++ include/domains/IntDomainsRepresentations.h
@@ -0,0 +1,61 @@
+#pragma once
+
+#include <data_structures/Vector.h>
+#include <utils/Utils.h>
+
+struct IntDomainsRepresentations
+{
+    Vector<int> minimums;
+    Vector<int> maximums;
+    Vector<int> offsets;
+    Vector<unsigned int> versions;
+    Vector<Vector<unsigned int>> bitvectors;
+
+    void initialize(int count);
+    void deinitialize();
+
+    void push(int min, int max);
+    void push(int min, int max, int offset, unsigned int version, Vector<unsigned int>* bitvector);
+    void pop();
+
+    inline int getChunkIndex(int index, int val)
+    {
+        return abs(val - offsets[index]) / UINT_BIT_SIZE;
+    }
+
+    inline int getBitIndex(int index, int val)
+    {
+        return abs(val - offsets[index]) % UINT_BIT_SIZE;
+    }
+
+    inline int getValue(int index, int chunkIndex, int bitIndex)
+    {
+        return offsets[index] + (UINT_BIT_SIZE * chunkIndex) + bitIndex;
+    }
+
+    inline bool isEmpty(int index)
+    {
+        return minimums[index] > maximums[index];
+    }
+
+    inline bool isSingleton(int index)
+    {
+        return minimums[index] == maximums[index];
+    }
+
+    inline unsigned int getApproximateCardinality(int index)
+    {
+        return abs(maximums[index] - minimums[index]) + 1;
+    }
+
+    bool contain(int index, int val);
+    bool getNextValue(int index, int val, int* nextVal);
+    bool getPrevValue(int index, int val, int* prevVal);
+
+    void remove(int index, int val);
+    void removeAnyLesserThan(int index, int val);
+    void removeAnyGreaterThan(int index, int val);
+
+    void removeAll(int index);
+    void keepOnly(int index, int val);
+};
---
+++ src/domains/IntDomainsRepresentations.cu
@@ -0,0 +1,252 @@
+#include <domains/IntDomainsRepresentations.h>
+#include <utils/Utils.h>
+
+void IntDomainsRepresentations::initialize(int count)
+{
+    minimums.initialize(count);
+    maximums.initialize(count);
+    offsets.initialize(count);
+    versions.initialize(count);
+    bitvectors.initialize(count);
+}
+
+void IntDomainsRepresentations::deinitialize()
+{
+    minimums.deinitialize();
+    maximums.deinitialize();
+    offsets.deinitialize();
+    versions.deinitialize();
+
+    for (int i = 0; i < bitvectors.size; i += 1)
+    {
+        bitvectors[i].deinitialize();
+    }
+    bitvectors.initialize();
+}
+
+void IntDomainsRepresentations::push(int min, int max)
+{
+    minimums.push_back(min);
+    maximums.push_back(max);
+    offsets.push_back(min);
+    versions.push_back(0);
+
+    bitvectors.resize_by_one();
+    int index = minimums.size - 1;
+    int maxChunkIndex = getChunkIndex(index, maximums[index]);
+    int chunksCount = maxChunkIndex + 1;
+    bitvectors.back().initialize(chunksCount);
+    bitvectors.back().resize(chunksCount);
+    AlgoUtils::fill(&bitvectors.back(), UINT_MAX);
+    int maxBitIndex = getBitIndex(index, maximums[index]);
+    unsigned int mask = BitsUtils::getLeftFilledMask(maxBitIndex);
+    bitvectors.back()[maxChunkIndex] &= mask;
+}
+
+void IntDomainsRepresentations::push(int min, int max, int offset, unsigned int version, Vector<unsigned int>* bitvector)
+{
+    minimums.push_back(min);
+    maximums.push_back(max);
+    offsets.push_back(offset);
+    versions.push_back(version);
+    bitvectors.resize_by_one();
+    bitvectors.back().initialize(bitvector);
+}
+
+void IntDomainsRepresentations::pop()
+{
+    minimums.pop_back();
+    maximums.pop_back();
+    offsets.pop_back();
+    versions.pop_back();
+    bitvectors.back().deinitialize();
+    bitvectors.pop_back();
+}
+
+bool IntDomainsRepresentations::contain(int index, int val)
+{
+    if (minimums[index] <= val and val <= maximums[index])
+    {
+        int valChunkIndex = getChunkIndex(index, val);
+        int valBitIndex = getBitIndex(index, val);
+
+        unsigned int mask = BitsUtils::getMask(valBitIndex);
+        return (bitvectors[index][valChunkIndex] & mask) != 0;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+bool IntDomainsRepresentations::getNextValue(int index, int val, int* nextVal)
+{
+    if (val < maximums[index])
+    {
+        int nextValChunkIndex = getChunkIndex(index, val);
+        int valBitIndex = getBitIndex(index, val);
+
+        unsigned int mask = ~BitsUtils::getLeftFilledMask(valBitIndex);
+        unsigned int nextValChunk = bitvectors[index][nextValChunkIndex] & mask;
+
+        if (nextValChunk == 0)
+        {
+            int maxChunkIndex = getChunkIndex(index, maximums[index]);
+            for (nextValChunkIndex = nextValChunkIndex + 1; nextValChunkIndex <= maxChunkIndex; nextValChunkIndex += 1)
+            {
+                if (bitvectors[index][nextValChunkIndex] != 0)
+                {
+                    break;
+                }
+            }
+
+            nextValChunk = bitvectors[index][nextValChunkIndex];
+        }
+
+        *nextVal = getValue(index, nextValChunkIndex, BitsUtils::getLeftmostOneIndex(nextValChunk));
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+bool IntDomainsRepresentations::getPrevValue(int index, int val, int* prevVal)
+{
+    if (minimums[index] < val)
+    {
+        int prevValChunkIndex = getChunkIndex(index, val);
+        int valBitIndex = getBitIndex(index, val);
+
+        unsigned int mask = ~BitsUtils::getRightFilledMask(valBitIndex);
+        unsigned int prevValChunk = bitvectors[index][prevValChunkIndex] & mask;
+
+        if (prevValChunk == 0)
+        {
+            int minChunkIndex = getChunkIndex(index, minimums[index]);
+            for (prevValChunkIndex = prevValChunkIndex - 1; prevValChunkIndex >= minChunkIndex; prevValChunkIndex -= 1)
+            {
+                if (bitvectors[index][prevValChunkIndex] != 0)
+                {
+                    break;
+                }
+            }
+
+            prevValChunk = bitvectors[index][prevValChunkIndex];
+        }
+
+        *prevVal = getValue(index, prevValChunkIndex, BitsUtils::getRightmostOneIndex(prevValChunk));
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+void IntDomainsRepresentations::remove(int index, int val)
+{
+    if (contain(index, val))
+    {
+        int valChunkIndex = getChunkIndex(index, val);
+        int valBitIndex = getBitIndex(index, val);
+
+        unsigned int mask = ~BitsUtils::getMask(valBitIndex);
+
+        bitvectors[index][valChunkIndex] &= mask;
+
+        if (not isEmpty(index))
+        {
+            if (val == minimums[index])
+            {
+                if(not getNextValue(index, val, &minimums[index]))
+                {
+                  removeAll(index);
+                }
+            }
+
+            if (val == maximums[index])
+            {
+                if(not getPrevValue(index, val, &maximums[index]))
+                {
+                    removeAll(index);
+                }
+            }
+        }
+
+        versions[index] += 1;
+    }
+}
+
+void IntDomainsRepresentations::removeAnyLesserThan(int index, int val)
+{
+    if (minimums[index] < val and val <= maximums[index])
+    {
+        int newMinimum = minimums[index];
+
+        if (contain(index, val))
+        {
+            newMinimum = val;
+        }
+        else
+        {
+            getNextValue(index, val, &newMinimum);
+        }
+
+        minimums[index] = newMinimum;
+
+        versions[index] += 1;
+    }
+    else if (val > maximums[index])
+    {
+        removeAll(index);
+    }
+}
+
+
+void IntDomainsRepresentations::removeAnyGreaterThan(int index, int val)
+{
+    if (minimums[index] <= val and val < maximums[index])
+    {
+        int newMaximum = maximums[index];
+
+        if (contain(index, val))
+        {
+            newMaximum = val;
+        }
+        else
+        {
+            getPrevValue(index, val, &newMaximum);
+        }
+
+        maximums[index] = newMaximum;
+
+        versions[index] += 1;
+    }
+    else if (val < minimums[index])
+    {
+        removeAll(index);
+    }
+}
+
+void IntDomainsRepresentations::keepOnly(int index, int val)
+{
+    if (contain(index, val))
+    {
+        minimums[index] = val;
+        maximums[index] = val;
+        versions[index] += 1;
+    }
+    else
+    {
+        removeAll(index);
+    }
+}
+
+void IntDomainsRepresentations::removeAll(int index)
+{
+    minimums[index] = INT_MAX;
+    maximums[index] = INT_MIN;
+    versions[index] += 1;
+}
