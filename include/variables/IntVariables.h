2c27a2bf98f9d5da59b9d7ccaeaa19faf892d03d
95A31 <x95a31xreg@gmail.com>
2017-09-25 00:09:29 +0200

    [Variables] Add base implementation of integer variables

--- include/variables/IntVariables.h
+++ include/variables/IntVariables.h
@@ -1,10 +1,17 @@
 #pragma once
 
 #include <data_structures/Vector.h>
+#include <domains/IntDomains.h>
 
 struct IntVariables
 {
     int count;
 
+    IntDomains domains;
     Vector<Vector<int>> constraints;
+
+    void initialize(int count);
+    void deinitialize();
+
+    void push(int min, int max);
 };
--- src/variables/IntVariables.cu
+++ src/variables/IntVariables.cu
@@ -1,1 +1,31 @@
 #include <variables/IntVariables.h>
+
+void IntVariables::initialize(int count)
+{
+   this->count = 0;
+
+   domains.initialize(count);
+   constraints.initialize(count);
+
+}
+
+void IntVariables::deinitialize()
+{
+    domains.deinitialize();
+
+    for(int i = 0; i < constraints.size; i += 1)
+    {
+        constraints[i].deinitialize();
+    }
+    constraints.deinitialize();
+}
+
+void IntVariables::push(int min, int max)
+{
+    domains.push(min, max);
+
+    constraints.resize_by_one();
+    constraints.back().initialize();
+
+    count += 1;
+}
