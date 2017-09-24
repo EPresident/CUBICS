2ee82453c4aea008610d09473cb4b729514bf92f
95A31 <x95a31xreg@gmail.com>
2017-09-24 22:09:39 +0200

    [Constraints] Add base implementation of integer constraints

--- include/constraints/IntConstraints.h
+++ include/constraints/IntConstraints.h
@@ -1,11 +1,26 @@
 #pragma once
 
 #include <data_structures/Vector.h>
+#include <variables/IntVariables.h>
 
 struct IntConstraints
 {
+    enum Type
+    {
+    };
+
     int count;
+
+    Vector<int> types;
 
     Vector<Vector<int>> variables;
     Vector<Vector<int>> parameters;
+
+    void initialize();
+    void deinitialize();
+
+    void push(int type);
+
+    void propagate(int index, IntVariables* variables);
+    bool satisfied(int index, IntVariables* variables);
 };
--- src/constraints/IntConstraints.cu
+++ src/constraints/IntConstraints.cu
@@ -1,1 +1,52 @@
 #include <constraints/IntConstraints.h>
+#include <utils/Utils.h>
+
+void IntConstraints::initialize()
+{
+    count = 0;
+
+    types.initialize();
+
+    variables.initialize();
+    parameters.initialize();
+}
+
+void IntConstraints::push(int type)
+{
+    types.push_back(type);
+
+    variables.resize_by_one();
+    variables.back().initialize();
+
+    parameters.resize_by_one();
+    parameters.back().initialize();
+
+    count += 1;
+}
+
+void IntConstraints::deinitialize()
+{
+    types.deinitialize();
+
+    variables.deinitialize();
+    parameters.deinitialize();
+}
+
+void IntConstraints::propagate(int index, IntVariables* variables)
+{
+    switch (types[index])
+    {
+        default:
+            LogUtils::error(__PRETTY_FUNCTION__, "Invalid constraint type");
+    }
+}
+
+bool IntConstraints::satisfied(int index, IntVariables* variables)
+{
+    switch (types[index])
+    {
+        default:
+            LogUtils::error(__PRETTY_FUNCTION__, "Invalid constraint type");
+            return false;
+    }
+}
