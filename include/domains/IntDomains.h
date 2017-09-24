09b04ab2256644cdd9b5fd09b62a1bc073758b3e
95A31 <x95a31xreg@gmail.com>
2017-09-24 22:56:27 +0200

    [Domains] Add base implementation of integer domains

--- include/domains/IntDomains.h
+++ include/domains/IntDomains.h
@@ -1,5 +1,53 @@
 #pragma once
 
+#include <domains/IntDomainsRepresentations.h>
+#include <domains/IntDomainsActions.h>
+
 struct IntDomains
 {
+    enum EventTypes
+    {
+        None,
+        Changed
+    };
+
+    Vector<int> events;
+
+    IntDomainsRepresentations representations;
+    IntDomainsActions actions;
+
+    void initialize(int count);
+    void deinitialize();
+
+    void push(int min, int max);
+
+
+    inline bool isEmpty(int index)
+    {
+        return representations.isEmpty(index);
+    }
+
+    inline bool isSingleton(int index)
+    {
+        return representations.isSingleton(index);
+    }
+
+    inline unsigned int getApproximateCardinality(int index)
+    {
+        return representations.getApproximateCardinality(index);
+    }
+
+    inline int getMin(int index)
+    {
+        return representations.minimums[index];
+    }
+
+    inline int getMax(int index)
+    {
+        return representations.maximums[index];
+    }
+
+    void fixValue(int index, int value);
+
+    void updateDomain(int index);
 };
--- src/domains/IntDomains.cu
+++ src/domains/IntDomains.cu
@@ -1,1 +1,58 @@
 #include <domains/IntDomains.h>
+
+void IntDomains::initialize(int count)
+{
+    events.initialize(count);
+
+    representations.initialize(count);
+    actions.initialize(count);
+}
+
+void IntDomains::deinitialize()
+{
+    events.deinitialize();
+
+    representations.deinitialize();
+    actions.deinitialize();
+}
+
+void IntDomains::push(int min, int max)
+{
+    events.push_back(EventTypes::Changed);
+
+    representations.push(min, max);
+    actions.push();
+}
+
+void IntDomains::fixValue(int index, int value)
+{
+    assert(representations.contain(index, value));
+
+    representations.keepOnly(index, value);
+    events[index] = EventTypes::Changed;
+}
+
+void IntDomains::updateDomain(int index)
+{
+    unsigned int previousVersion = representations.versions[index];
+
+    representations.removeAnyGreaterThan(index, actions.upperbounds[index]);
+
+    representations.removeAnyLesserThan(index, actions.lowerbounds[index]);
+
+    for (int ei = 0; ei < actions.elementsToRemove[index].size; ei += 1)
+    {
+        representations.remove(index, actions.elementsToRemove[index][ei]);
+    }
+
+    actions.clear(index);
+
+    if (previousVersion != representations.versions[index])
+    {
+        events[index] = EventTypes::Changed;
+    }
+    else
+    {
+        events[index] = EventTypes::None;
+    }
+}
