7064ea4ad584c1ddd23b925dc48ed8dd2f9d9376
95A31 <x95a31xreg@gmail.com>
2017-12-02 09:00:07 +0100

    [Utils] Update bits utils to CUDA intrinsic

--- include/utils/BitUtils.h
+++ include/utils/BitUtils.h
@@ -8,17 +8,29 @@ namespace BitsUtils
 {
     inline int getLeftmostOneIndex(unsigned int val)
     {
+#ifdef __CUDA_ARCH__
+        return __clz(val);
+#else
         return __builtin_clz(val);
+#endif
     }
 
     inline int getRightmostOneIndex(unsigned int val)
     {
+#ifdef __CUDA_ARCH__
+        return UINT_BIT_SIZE - __ffs(val);
+#else
         return UINT_BIT_SIZE - __builtin_ffs(val);
+#endif
     }
 
     inline int getOnesCount(unsigned int val)
     {
+#ifdef GPU
+        return __popc(val);
+#else
         return __builtin_popcount(val);
+#endif
     }
 
     inline unsigned int getMask(int bitIndex)
